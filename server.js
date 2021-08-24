390fc19721de3bf5c17803ad064f2a0f29000775
Mahmoud_Fady <58427542+MahmoudFady@users.noreply.github.com>
2021-08-25 01:59:47 +0200

    -v

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ const PORT = process.env.PORT || 3200;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log("server is running on port : " + PORT);
+  console.log("server working ....");
 });
