77d6e7b9f6c410cfa5774664237246f9fb097319
Mahmoud_Fady <58427542+MahmoudFady@users.noreply.github.com>
2021-09-12 19:33:59 +0200

    v2

--- backend/controller/comment.js
+++ backend/controller/comment.js
@@ -1,21 +1,28 @@
 const Comment = require("../model/comment");
 const Product = require("../model/product");
 module.exports.addComment = async (decode, req, res, next) => {
+  console.log("addcoment");
   const { productId } = req.params;
   const { content } = req.body;
   const creator = decode.userId;
-  const newComment = await new Comment({ creator, content }).save();
+  let newComment = await new Comment({ creator, content }).save();
+  newComment = await Comment.findById(newComment._id).populate({
+    path: "creator",
+  });
   const product = await Product.findOne({ _id: productId });
-  const oldProductComments = product.comments;
+  let oldProductComments = product.comments;
   let newProductComments = [];
   if (oldProductComments.length == 0) {
     newProductComments = [newComment._id];
   } else {
-    newProductComments = oldProductComments.push(newComment._id);
+    oldProductComments.push(newComment._id);
+    newProductComments = oldProductComments;
   }
-  const newProduct = await Product.updateOne(
-    { _id: productId }, 
-    { $set: { comments: newProductComments } }
-  );
-  res.status(200).json({ message: "successfully comment added", newProduct });
+  await Product.findByIdAndUpdate(productId, {
+    $set: { comments: newProductComments },
+  });
+  res.status(200).json({
+    message: "successfully comment added",
+    newComment,
+  });
 };
--- backend/controller/dist/comment.dev.js
+++ backend/controller/dist/comment.dev.js
@@ -5,28 +5,36 @@ var Comment = require("../model/comment");
 var Product = require("../model/product");
 
 module.exports.addComment = function _callee(decode, req, res, next) {
-  var productId, content, creator, newComment, product, oldProductComments, newProductComments, newProduct;
+  var productId, content, creator, newComment, product, oldProductComments, newProductComments;
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
+          console.log("addcoment");
           productId = req.params.productId;
           content = req.body.content;
           creator = decode.userId;
-          _context.next = 5;
+          _context.next = 6;
           return regeneratorRuntime.awrap(new Comment({
             creator: creator,
             content: content
           }).save());
 
-        case 5:
+        case 6:
           newComment = _context.sent;
-          _context.next = 8;
+          _context.next = 9;
+          return regeneratorRuntime.awrap(Comment.findById(newComment._id).populate({
+            path: "creator"
+          }));
+
+        case 9:
+          newComment = _context.sent;
+          _context.next = 12;
           return regeneratorRuntime.awrap(Product.findOne({
             _id: productId
           }));
 
-        case 8:
+        case 12:
           product = _context.sent;
           oldProductComments = product.comments;
           newProductComments = [];
@@ -34,26 +42,24 @@ module.exports.addComment = function _callee(decode, req, res, next) {
           if (oldProductComments.length == 0) {
             newProductComments = [newComment._id];
           } else {
-            newProductComments = oldProductComments.push(newComment._id);
+            oldProductComments.push(newComment._id);
+            newProductComments = oldProductComments;
           }
 
-          _context.next = 14;
-          return regeneratorRuntime.awrap(Product.updateOne({
-            _id: productId
-          }, {
+          _context.next = 18;
+          return regeneratorRuntime.awrap(Product.findByIdAndUpdate(productId, {
             $set: {
               comments: newProductComments
             }
           }));
 
-        case 14:
-          newProduct = _context.sent;
+        case 18:
           res.status(200).json({
             message: "successfully comment added",
-            newProduct: newProduct
+            newComment: newComment
           });
 
-        case 16:
+        case 19:
         case "end":
           return _context.stop();
       }
